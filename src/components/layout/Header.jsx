93022939236ab9361c129f9b6f52ab33e458af83
Shrawan kr kamat <shrawam.fontend.bmdu@gmail.com>
2026-02-03 12:42:31 +0530

    updated

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -8,7 +8,7 @@ import cartIcon from '../../assets/SVG.svg';
 import userIcon from '../../assets/icon 6.svg';
 import locationIcon from '../../assets/Icon 2.svg';
 import searchIcon from '../../assets/Icon 3.svg';
-import dropdownIcon from '../../assets/Icon 4.svg';
+import dropdownIcon from '../../assets/icon 4.svg';
 import wishlistIcon from '../../assets/icon 5.svg';
 
 function Header() {
