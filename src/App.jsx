cccf6a0cecc7ee69422f70123d01fe2b3c5a8624
Shrawan kr kamat <shrawam.fontend.bmdu@gmail.com>
2026-02-19 11:17:44 +0530

    updtaed

--- src/App.jsx
+++ src/App.jsx
@@ -27,29 +27,33 @@ function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product-details" element={<ProductDetailPage />} />
-          <Route path="/category/:categoryName" element={<ProductListingPage />} />
-          <Route path="/about-us" element={<AboutUsPage />} />
-          <Route path="/my-profile" element={<MyProfilePage />} />
-          <Route path="/my-orders" element={<MyOrdersPage />} />
-          <Route path="/wishlist" element={<WishlistPage />} />
-          <Route path="/addresses" element={<SavedAddressPage />} />
-          <Route path="/payment" element={<PaymentPage />} />
-          <Route path="/careers" element={<CareerPage />} />
-          <Route path="/press" element={<PressPage />} />
-          <Route path="/partners" element={<PartnersPage />} />
-          <Route path="/investor-relations" element={<InvestorRelationsPage />} />
-          <Route path="/corporate-sales" element={<CorporateSalesPage />} />
-          <Route path="/help-support" element={<HelpSupportPage />} />
-          <Route path="/contact-us" element={<ContactUsPage />} />
-          <Route path="/cart" element={<CartPage />} />
-           <Route path="/checkout" element={<Checkout/>} />
-          <Route path="/orderaddress" element={<OrderAddress/>} />
-           <Route path="/search" element={<SearchPage />} />
-          <Route path="/secendaddress" element={<SecendAddress/>} />
-           <Route path="/pay" element={<Pay />} />
-             <Route path="/order-confirm" element={<OrderConfirmPage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product-details" element={<ProductDetailPage />} />
+        <Route
+          path="/category/:categoryName"
+          element={<ProductListingPage />}
+        />
+        <Route path="/about-us" element={<AboutUsPage />} />
+        <Route path="/my-profile" element={<MyProfilePage />} />
+        <Route path="/my-orders" element={<MyOrdersPage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
+        <Route path="/addresses" element={<SavedAddressPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/careers" element={<CareerPage />} />
+        <Route path="/press" element={<PressPage />} />
+        <Route path="/partners" element={<PartnersPage />} />
+        <Route path="/investor-relations" element={<InvestorRelationsPage />} />
+        <Route path="/corporate-sales" element={<CorporateSalesPage />} />
+        <Route path="/help-support" element={<HelpSupportPage />} />
+        <Route path="/contact-us" element={<ContactUsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/orderaddress" element={<OrderAddress />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/secendaddress" element={<SecendAddress />} />
+
+        <Route path="/pay" element={<Pay />} />
+        <Route path="/order-confirm" element={<OrderConfirmPage />} />
       </Routes>
     </Router>
   );
